a55e2432296ce90adc341769c46ca87aa7257a70
HlobisileZulu <hlobiemageba@gmail.com>
2026-05-12 12:38:41 +0200

    Update CartItem.jsx

--- react/src/components/CartItem.jsx
+++ react/src/components/CartItem.jsx
@@ -7,7 +7,8 @@ export default function CartItem(){
  const dispatch=useDispatch();
  const totalQty=items.reduce((t,i)=>t+i.qty,0);
  const total=items.reduce((t,i)=>t+i.qty*i.price,0);
- return <div><Header /><h2>Cart ({totalQty})</h2><h3>Total: R{total}</h3>{items.map(i=><div className='card' key={i.id}><img src={i.img}/><h3>{i.name}</h3><p>Unit: R{i.price}</p><p>Subtotal: R{i.qty*i.price}</p><button onClick={()=>dispatch(increase(i.id))}>+</button><span>{i.qty}</span><button onClick={()=>dispatch(decrease(i.id))}>-</button><button onClick={()=>dispatch(remove(i.id))}>Delete</button></div>)}<button onClick={()=>alert('Coming Soon')}>Checkout</button><Link to='/plants'><button>Continue Shopping</button></Link></div>
+ return <div><Header />
+<h2>Total: R{calculateTotalAmount()}</h2><h3>Total: R{total}</h3>{items.map(i=><div className='card' key={i.id}><img src={i.img}/><h3>{i.name}</h3><p>Unit: R{i.price}</p><p>Subtotal: R{i.qty*i.price}</p><button onClick={()=>dispatch(increase(i.id))}>+</button><span>{i.qty}</span><button onClick={()=>dispatch(decrease(i.id))}>-</button><button onClick={()=>dispatch(remove(i.id))}>Delete</button></div>)}<button onClick={()=>alert('Coming Soon')}>Checkout</button><Link to='/plants'><button>Continue Shopping</button></Link></div>
 }
 
   const calculateTotalAmount = () => {
